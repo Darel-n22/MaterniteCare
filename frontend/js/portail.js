975b46a109958c2bde8a4ec3269812938390c616
Darel-n22 <darelnsika@gmail.com>
2026-06-21 19:06:52 +0100

    Correction API_URL pour Render

--- frontend/js/portail.js
+++ frontend/js/portail.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://maternitecare-api.onrender.com/';
+const API_URL = 'https://maternitecare-api.onrender.com';
 let currentPatient = null;
 
 // ---------- NOTIFICATIONS TOAST ----------
